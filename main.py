90bb59a1db8a23f23eeb7d873caa7b3c8ebf450a
KamilJ <kamilj@wsb.wroclaw.pl>
2022-03-06 14:34:52 +0100

    Zadanie 1.2a/1.2b usuwanie kropek/przecinkwo - done

--- main.py
+++ main.py
@@ -16,13 +16,26 @@ with open(path, 'r') as file:
 #1.2a zamiana na male litery
 #1.2b usuwanie kropek, przecinkow
 
-#1.2a
+#1.2a zamiana na male litery
     with open(path, 'r') as file:
         content_lista = str(content)
         content_male_litery = content_lista.lower()
         print (content_male_litery)
 
-#1.2b
+#1.2b usuwanie kropek, przecinkow
+    def usuwator_kropek(content):
+
+        x = content.replace(".", "")
+        return x
+        print(usuwator_kropek(content))
+
+#1.2c usuwanie przecinkow
+    def usuwator_przecinkow(content):
+        x = content.replace(",", "")
+        return x
+        print(usuwator_przecinkow(content))
+
+
 
 
 
