7785ce9f075bda6832b5b3072a3b9b92b905b5e7
KamilJ <kamilj@wsb.wroclaw.pl>
2022-03-06 13:35:43 +0100

    Zad.1.1 czytamy plik tekstowy - done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#test commita
+# Napisać program, który:
+# # - czyta plik tekstowy
+# # - usuwa kropki, przecinki,
+# # zamienia litery na małe
+# # - liczy liczbę różnych słów
+# # - liczy liczbę powtórzeń danych slow
+
+#1.1 czytamy plik tekstowy
+
+path = 'C:\\Users\\yer0nos\\zadanie3\\rolling_stones.txt'
+with open(path, 'r') as file:
+    content = file.readlines()
+
+
+
