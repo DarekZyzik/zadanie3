71bef897bc916c4ec6f7b611658a1b962eaeaad8
KamilJ <kamilj@wsb.wroclaw.pl>
2022-03-06 13:49:20 +0100

    Zamiana na male litery - done

--- main.py
+++ main.py
@@ -10,6 +10,22 @@
 path = 'C:\\Users\\yer0nos\\zadanie3\\rolling_stones.txt'
 with open(path, 'r') as file:
     content = file.readlines()
+    content_lista = file.read()
+    print (content)
+
+#1.2a zamiana na male litery
+#1.2b usuwanie kropek, przecinkow
+
+#1.2a
+    with open(path, 'r') as file:
+        content_lista = str(content)
+        content_male_litery = content_lista.lower()
+        print (content_male_litery)
+
+#1.2b
+
+
+
 
 
 
